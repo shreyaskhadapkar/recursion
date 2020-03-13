edb4f92d8a5e1fe627ac93cbc8f9a8ff76a518bd
Srajan1122 <mahabala1122@gmail.com>
2020-03-14 03:25:25 +0530

    format of user view

--- recursion/userView/urls.py
+++ recursion/userView/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('Home/',views.homepage,name='homepage'),
-    
-   
-
+    path('scanner/',views.scanner,name='scanner'),
+    path('cart/',views.cart,name='cart')
 ]
--- recursion/userView/views.py
+++ recursion/userView/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render,redirect
 from .models import Product
 from django.urls import reverse
 
-# Create your views here.
-
 def homepage(request):
     return render(request,'userView/Homepage.html')
+
+def scanner(request):
+    return render(request,'userView/qrcode.html')
+
+def cart(request):
+    return render(request,'userView/cart.html')
