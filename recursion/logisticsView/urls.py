a846921d6d64276d6baf2e47edff53bcab6cfb81
Srajan1122 <mahabala1122@gmail.com>
2020-03-13 21:35:04 +0530

    backend of logistic view

--- recursion/logisticsView/urls.py
+++ recursion/logisticsView/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('product/',views.product,name='product')
+    path('product/',views.product,name='product'),
+    path('productUpdate/<slug:slug>/',views.productUpdate,name='productUpdate'),
+    path('productAdd/',views.productAdd,name='productAdd')
 ]
